2a3300f7394cdff52602727510272ef7751c8453
Oğuzhan <oguzhnn.yavuz@protonmail.com>
2021-12-15 22:49:01 +0000

    Update nodemcu_webserver_with_websocket.ino

--- nodemcu_webserver_with_websocket.ino
+++ nodemcu_webserver_with_websocket.ino
@@ -1,10 +1,3 @@
-/*********
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp8266-nodemcu-websocket-server-arduino/
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*********/
-
 // Import required libraries
 #include "src/env/env.h"
 #include "src/web_socket/web_socket.h"
